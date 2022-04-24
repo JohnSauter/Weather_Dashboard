8815b4cc4827f0bb4c0cb053700b1b8f256a40bc
John Sauter <John_Sauter@systemeyescomputerstore.com>
2022-04-23 20:18:20 -0400

    It appears to be working

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # UNH_bootcamp-06-Weather_Dashboard
 Display the weather at a specified location
+
+Type a place name in the box below "Search for a City"
+and the right side of the form will show weather information
+for that city, including a five-day forecast.  Each city
+named is added to the history list below the input box.
+These names are kept in order by most recently used,
+and persist across running the application.  Clicking
+on a city on the list will display its weather and
+bring it to the top of the list.
+
+The application is at github at this URL:
+https://github.com/JohnSauter/UNH_bootcamp-06-Weather_Dashboard
+
+Here is a screen shot showing the weather in Atlanta, GA:
+
+![screen shot of Weather Dashboard](./assets/images/Weather_Dashboard.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -27,8 +27,6 @@ function handle_submit(event) {
   const target = $(event.target);
   const target_input = target.children("input");
   let city_name = target_input.val();
-  /* Remove spaces, since we will be constructing a URL with
-   * the name.  */
   remember_city_name(city_name);
   target_input.val("");
 }
@@ -44,19 +42,22 @@ function remember_city_name (city_name) {
     city_names_data[city_name] = {"last_used": date_stamp};
 
     /* Save the updated list of cities in local storage.  */
-    localStorage.setItem("weather_dashboard", JSON.stringify(city_names_data));
+    localStorage.setItem("weather_dashboard", 
+      JSON.stringify(city_names_data));
   
     /* Update the displayed list of cities.  */
     reload_city_names();
 
-    /* Process the specified city name.  */
+    /* Process the specified city name.  This function is
+     * asynchronous so we must exit and let the completion
+     * of its promises carry on.  */
     process_city_name(city_name);
   }
 
-/* Function to load the saved city names onto the display.  */
+/* Function to display the saved city names.  */
 function reload_city_names () {
   /* Sort the city names by least recently used, so the
-   * oldest ones disappear off the list.  */
+   * oldest ones disappear off the bottom of the list.  */
   const entries = Object.entries(city_names_data);
   const sorted_entries = entries.sort(
     function (a, b) {
@@ -66,7 +67,7 @@ function reload_city_names () {
       return 0;
     }
   )
-  /* There is a line for each city name.  */
+  /* There is a line for each displayable city name.  */
   for (let i = 0; i < sorted_entries.length; i++) {
     const row_id = String(i).padStart(2, "0");
     const row_element = $("#city_row_" + row_id);
@@ -80,16 +81,19 @@ function reload_city_names () {
 $("#city_buttons").on("click", handle_city_button);
 function handle_city_button (event) {
   const target = $(event.target);
-  process_city_name(target.text());
+  remember_city_name(target.text());
 }
 
-/* Function to add the location to a city name.  */
+/* Function to add the location to a city name.  
+ * This is an asynchronous function, so it will return
+ * before completing the update to the city_names_data object.  */
 async function add_location (city_name) {
   const URL_start = "http://api.openweathermap.org/geo/1.0/direct?q=";
   const URL_end = "&appid=";
 
   /* Construct the URL.  */
-  const the_URL = URL_start + city_name + URL_end + api_key;
+  const the_URL = URL_start + encodeURIComponent (city_name) + URL_end + 
+    encodeURIComponent(api_key);
 
   /* Fetch the location of the city.  */
   console.log(the_URL);
@@ -102,9 +106,9 @@ async function add_location (city_name) {
    * for it.  */
   localStorage.setItem("weather_dashboard", 
     JSON.stringify(city_names_data));
-  /* Because this function is asynchronous we cannot call it
+  /* Because add location is asynchronous we cannot call it
    * and expect it to be dome when it returns.  Therefore
-   * when it completes it calls process_city_name, which
+   * when it completes, it calls process_city_name, which
    * was its caller.  Process_city_name will find the location
    * in the city_names_data structure, and so not call 
    * add_location again.
@@ -137,13 +141,16 @@ function process_city_name (city_name) {
    * and longitude of the location for which we want
    * the weather.  */
   const URL_start = "http://api.openweathermap.org/data/2.5/onecall";
-  const the_URL = URL_start + "?lat=" + latitude + 
-    "&lon=" + longtitude + "&appid=" + api_key;
+  const the_URL = URL_start + "?lat=" + encodeURIComponent(latitude) + 
+    "&lon=" + encodeURIComponent(longtitude) + "&appid=" + 
+      encodeURIComponent(api_key);
     fetch(the_URL)
     .then(function(response) {return response.json()})
     .then(function (data) {
       /* We have the weather data.  Store it and display it.  */
       city_names_data[city_name].weather = data;
+      localStorage.setItem("weather_dashboard", 
+        JSON.stringify(city_names_data));
       display_city_weather (city_name)});
   } else {
   /* We must fetch the city's location.  This process
@@ -165,21 +172,92 @@ function display_city_weather (city_name) {
   const weather_data = city_names_data[city_name].weather;
   $("#display_city_name").text(location_data[0].name);
   const the_time_t = weather_data.current.dt;
-  const the_date = moment.unix(the_time_t).format("dddd, MMMM Do YYYY, HH:mm:ss");
+  const the_date = moment.unix(the_time_t).format("[(]M[/]D[/]YYYY[)]");
   $("#display_date").text(the_date);
-  const weather_description = weather_data.current.weather[0].description;
   const weather_icon = weather_data.current.weather[0].icon;
   const weather_icon_URL = "https://openweathermap.org/img/wn/" +
     weather_icon + ".png";
-  $("#display_conditions").html("<span>" + weather_description + 
-    "<span> <img id='condition_image' src='" + weather_icon_URL + "'></span></span>");
-    const temp_Kelvin = weather_data.current.temp;
-    const temp_celcius = temp_Kelvin -273.15;
-    $("#display_temperature").text(temp_celcius.toFixed(2) + "°C");
-    const humidity = weather_data.current.humidity;
-    $("#display_humidity").text(humidity + "%");
-    const wind_speed = weather_data.current.wind_speed;
-    $("#display_wind_speed").text(wind_speed + " meters per second");
-    const UV_index = weather_data.current.uvi;
-    $("#display_UV_index").text(UV_index);
+  $("#display_conditions").html("<span>" + 
+    "<span> <img class='weather_condition_icon' src='" + weather_icon_URL + 
+    "'></span></span>");
+  const temp_Kelvin = weather_data.current.temp;
+  const temp_celsius = temp_Kelvin - 273.15;
+  const temp_farenheit = (temp_celsius * (9/5)) + 32.0;
+  $("#display_temperature").text(temp_celsius.toFixed(2) + " °C = " +
+    temp_farenheit.toFixed(2) + " °F");
+  const humidity = weather_data.current.humidity;
+  $("#display_humidity").text(humidity + "%");
+  const wind_speed = weather_data.current.wind_speed;
+  const wind_speed_mph = wind_speed * 2.236936292054402;
+  $("#display_wind_speed").text(wind_speed + " meters per second = " +
+    wind_speed_mph.toFixed(2) + " miles per hour");
+  const UV_index = weather_data.current.uvi;
+
+  /* Along with the UV index show a color which indicates the
+   * condition: low, moderate, high, very high or extreme.  */
+  let UV_index_color;
+  if (UV_index < 3) {
+     UV_index_color = "#80D07B"; /* green: low */
+  } else {
+    if (UV_index < 6) {
+      UV_index_color = "#FEDD03"; /* yellow: moderate */
+    } else {
+      if (UV_index < 8) {
+        UV_index_color = "#F79210"; /* orange: high */
+      } else {
+        if (UV_index < 11) {
+          UV_index_color = "#ED1C24"; /* red: very high */
+        } else {
+          UV_index_color = "#DB1C85"; /* violet: extreme */
+        }
+      }
+    }
+  }
+  const UV_index_text_1 = "<span> <svg height='20' width='20'>";
+  const UV_index_text_2 = " <circle cx='10' cy='10' r='10' stroke='black'";
+  const UV_index_text_3 = " stroke-width='2' fill='" + UV_index_color + "'";
+  const UV_index_text_4 = " /></svg></span>"
+  const UV_index_text = UV_index + UV_index_text_1 + UV_index_text_2 +
+    UV_index_text_3 + UV_index_text_4;
+  $("#display_UV_index").html(UV_index_text);
+
+  /* Now fill in the daily forecasts.  */
+  for (let day_no = 0; day_no < weather_data.daily.length; day_no++) {
+    daily_forecast (weather_data, day_no);
+  }
 }
+
+/* Function to display the forecast for each day.  */
+function daily_forecast (weather_data, day_no) {
+  const forecasts = weather_data.daily;
+  const this_forecast = forecasts[day_no];
+  const the_time_t = this_forecast.dt;
+  const the_date = moment.unix(the_time_t).format("M[/]D[/]YYYY");
+  const this_element = $("#forecast_" + String(day_no));
+  const this_body = this_element.children("div");
+  const this_title = this_body.children("h5");
+  this_title.text(the_date);
+  const weather_icon = this_forecast.weather[0].icon;
+  const weather_icon_URL = "https://openweathermap.org/img/wn/" +
+    weather_icon + ".png";
+  let forecast_body = "<p> <img class='weather_condition_icon' src='" + 
+    weather_icon_URL + "'></p>";
+  const temp_Kelvin_min = this_forecast.temp.min;
+  const temp_Kelvin_max = this_forecast.temp.max;
+  const temp_celsius_min = temp_Kelvin_min - 273.15;
+  const temp_celsius_max = temp_Kelvin_max - 273.15;
+  const temp_farenheit_min = (temp_celsius_min * (9/5)) + 32.0;
+  const temp_farenheit_max = (temp_celsius_max * (9/5)) + 32.0;
+  forecast_body = forecast_body + "<p>Temp: " + temp_farenheit_min.toFixed(2) +
+    "&ndash;" + temp_farenheit_max.toFixed(2) + " °F</p>";
+  const wind_speed = this_forecast.wind_speed;
+  const wind_speed_mph = wind_speed * 2.236936292054402;
+  forecast_body = forecast_body + "<p>Wind: " + wind_speed_mph.toFixed(2) + 
+    " MPH</p>";
+  const humidity = this_forecast.humidity;
+  forecast_body = forecast_body + "<p>Humidity: " + humidity + "%</p>";
+
+  this_text = this_body.children("p");
+  this_text.html(forecast_body);
+}
+
